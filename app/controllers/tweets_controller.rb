13ba5e3731878fb12a1c5b13d59de374c0eb02b3
kentaro <skyblow.9@gmail.com>
2025-05-12 00:34:30 +0900

    編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,37 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
+
+
 end
